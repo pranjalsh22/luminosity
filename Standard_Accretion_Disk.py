f2efbde4f0eedad44140502b877a2752354b237d
pranjalsh22 <physics0403@gmail.com>
2025-05-26 13:09:50 +0530

    Update Standard_Accretion_Disk.py

--- Standard_Accretion_Disk.py
+++ Standard_Accretion_Disk.py
@@ -441,19 +441,18 @@ def the_Frequency_vs_Luminosity_part2(p):
                            list(np.linspace(3e12, 4.3e14,20))+ \
                            list(np.linspace(4.3e14, 7.5e14,20))+ \
                            list(np.linspace(7.5e14, 3e16,20)))
+        frequencies=list(set(frequencies))
     else:
         frequencies=sorted([10**n for n in range(1,23)]+ \
                            [2*10**n for n in range (0,22)]+ \
                            [4*10**n for n in range (0,22)]+ \
                            [6*10**n for n in range (0,22)]+ \
-                           [7*10**n for n in range (0,22)]+ \
-                           [9*10**n for n in range (0,22)]+ \
                            [8*10**n for n in range (0,22)]+ \
                            list(np.linspace(1e-5*F1,100*F2,40))+ \
                            list(np.linspace(3e12, 4.3e14,20))+ \
                            list(np.linspace(4.3e14, 7.5e14,20))+ \
                            list(np.linspace(7.5e14, 3e16,20)))
-
+        frequencies=list(set(frequencies))
     #finding corresponding luminosity density for selected frequency list                      
     luminosities=[]
     for i in frequencies:
