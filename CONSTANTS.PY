a0d745b6180ef1832430d0921d9282f93054cc86
Pranjal Sharma <physics0403@gmail.com>
2026-01-25 15:37:00 +0530

    Create CONSTANTS.PY

---
+++ CONSTANTS.PY
@@ -0,0 +1,12 @@
+#constants
+global G,m_sun_kg, c, sbc,pi, h,k 
+G=6.67430e-11 #'''Nm2/kg2'''
+m_sun_kg = 1.988e30 #'''kg'''#defined again in functioning
+c=299792458 #'''m/s'''
+sbc=5.67e-8 #watt/m2K4
+pi=3.141592653589
+h=6.62607015e-34 #J/Hz
+k=1.380649e-23 #m2 kg /(Ks2)
+
+FO1=3.2928e15
+FO2=8.4922e15
